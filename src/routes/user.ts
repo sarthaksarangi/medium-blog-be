fdcccf614c72d5692effd7b090c086148cfe1be7
sarthaksarangi <sarthakrohan5@gmail.com>
2024-10-16 01:50:34 +0530

    Modified schema and added zod validation to user and blog routes

--- src/routes/user.ts
+++ src/routes/user.ts
@@ -2,6 +2,7 @@ import { Hono } from "hono";
 import { PrismaClient } from "@prisma/client/edge";
 import { withAccelerate } from "@prisma/extension-accelerate";
 import { sign } from "hono/jwt";
+import { signinInput, signupInput } from "@sarthak.dev/medium-common";
 
 export const userRouter = new Hono<{
   Bindings: {
@@ -19,9 +20,15 @@ userRouter.post("/signup", async (c) => {
   }).$extends(withAccelerate());
 
   const body = await c.req.json();
+  const { success } = signupInput.safeParse(body);
+  if (!success) {
+    c.status(411);
+    c.json({ error: "Incorrect input formatting" });
+  }
   try {
     const user = await prisma.user.create({
       data: {
+        name: body.name,
         email: body.email,
         password: body.password,
       },
@@ -41,8 +48,13 @@ userRouter.post("/signin", async (c) => {
   }).$extends(withAccelerate());
 
   const body = await c.req.json();
+  const { success } = signinInput.safeParse(body);
+  if (!success) {
+    c.status(411);
+    c.json({ error: "Incorrect input formatting" });
+  }
   const user = await prisma.user.findUnique({
-    where: { email: body.email },
+    where: { email: body.email, password: body.password },
   });
 
   if (!user) {
